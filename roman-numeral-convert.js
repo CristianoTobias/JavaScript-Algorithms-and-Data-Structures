ecd192d53e7243114549679729ce6f154e1c33c4
Cristiano Tobias <cristiano.tobias40@gmail.com>
2023-02-25 19:20:43 -0300

    up date

--- roman-numeral-convert.js
+++ roman-numeral-convert.js
@@ -25,35 +25,3 @@ function convertToRoman(num) {
 
 console.log(convertToRoman(3999))
 
-function rot13(str) {
-  let alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-  return str.split("").map(item =>
-  {
-     
-    if(alphabet.indexOf(item) >= 0){
-      let index
-      if(alphabet.indexOf(item) < 13){
-        index = 26 - (13 - (alphabet.indexOf(item)))
-      }else{
-        index = alphabet.indexOf(item) % 13
-      }
-      
-      return alphabet[index]
-    }else{
-      return item
-    }
-  
-  } ).join("")
-}
-
-
-console.log(rot13("GUR DHVPX OEBJA SBK WHZCF BIRE GUR YNML QBT."))
-
-function palindrome(str) {
-  const regex = new RegExp(/[\s_*#,.\\\(\):-]/g)
-  return str.toUpperCase().replace(regex, "").split("").reverse().join("") == str.replace(regex, "").toUpperCase()
-}
-
-
-
-console.log(palindrome('_eye'))
